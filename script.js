03f9b6f4152f324942f712aea74148ea7c0a5aff
Mark Lannen <Mark.Lannen@mumit-mdm17.dhcp.umt.edu>
2023-04-07 15:25:24 -0600

    add ability to store todos in local storage

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ const form = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUL = document.getElementById('todos')
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
     addTodo()
 })
 
 function addTodo(todo) {
-    let todoText = input.value;
+    let todoText = input.value
 
-    if(todo){
-        todoText = todo.text;
+    if(todo) {
+        todoText = todo.text
     }
 
     if(todoText) {
@@ -20,17 +26,40 @@ function addTodo(todo) {
         if(todo && todo.completed) {
             todoEl.classList.add('completed')
         }
-        todoEl.innerText = todoText;
 
-        todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'))
-        todoEl.addEventListener('contextmenu', (e) => {
-            e.preventDefault();
+        todoEl.innerText = todoText
 
-            todoEl.remove()
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed')
+            updateLS()
         })
 
-        todosUL.appendChild(todoEl);
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+
+            todoEl.remove()
+            updateLS()
+        })
+
+        todosUL.appendChild(todoEl)
 
         input.value = ''
+
+        updateLS()
     }
+}
+
+function updateLS() {
+    todosEl = document.querySelectorAll('li')
+
+    const todos = []
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
